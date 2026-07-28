ab4f5a71f8ed6be014f5af43451cdc91685f5f72
Leoplureodon <leoplureodon@gmail.com>
2026-07-28 12:45:12 +0100

    [fix] version compatibility updates, esling config update

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,9 @@
 import globals from "globals";
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import js from "@eslint/js";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
 
 export default [
   {
@@ -23,13 +23,13 @@ export default [
       react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
-      "@typescript-eslint": tsPlugin,
+      "@typescript-eslint": tseslint.plugin,
     },
     rules: {
       // spread JS recommended rules
       ...js.configs.recommended.rules,
       // spread TS recommended rules
-      ...tsPlugin.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
       // React + hooks rules
       ...react.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
